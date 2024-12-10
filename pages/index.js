9245f3311bceaa6d36b0c5e868a941bf7804ebf1
Carlos Barreto <129400612+dev-cerb@users.noreply.github.com>
2024-12-10 17:54:46 +0000

    feat:: `lint:fix and lint:check`

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,17 @@
-export default function Home(){
+export default function Home() {
+  function texto() {
+    const texto = document.querySelector(".texto");
+    texto.innerHTML +=
+      "Só para dizer o quanto você é especial, e que as dificuldades são só uma fase que vai passar. Te adoro <3";
+  }
 
-    function texto(){
-        const texto = document.querySelector(".texto");
-        texto.innerHTML += "Só para dizer o quanto você é especial, e que as dificuldades são só uma fase que vai passar. Te adoro <3"
-    }
-
-    return (
-        <div>
-            <button type="button" onClick={texto}>clica aqui</button>
-            <br></br>
-            <p className="texto"></p>
-        </div>
-
-    )
+  return (
+    <div>
+      <button type="button" onClick={texto}>
+        clica aqui
+      </button>
+      <br></br>
+      <p className="texto"></p>
+    </div>
+  );
 }
